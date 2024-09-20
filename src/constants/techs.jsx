6e0e51c6958f0ac35b43c609cdae4033809cda10
Shaqeal Jinnah <j.shaqeal@gmail.com>
2024-09-20 22:33:36 +1000

    update tech stack

--- src/constants/techs.jsx
+++ src/constants/techs.jsx
@@ -6,56 +6,57 @@ import reactIcon from "../assets/Icon/Tech/react.svg";
 import reduxIcon from "../assets/Icon/Tech/redux.svg";
 import firebaseIcon from "../assets/Icon/Tech/firebase.svg";
 import sassIcon from "../assets/Icon/Tech/sass.svg";
+import nextIcon from "../assets/Icon/Tech/next.svg";
 
 const techs = [
-    {
-      title: "HTML 5",
-      icon: htmlIcon,
-      detail:
-        "Hypertext Markup Language, used for structuring and presenting content on the World Wide Web.",
-    },
-    {
-      title: "CSS 3",
-      icon: cssIcon,
-      detail:
-        "Cascading Style Sheets, used for describing the look and formatting of a document written in HTML.",
-    },
-    {
-      title: "JavaScript",
-      icon: jsIcon,
-      detail:
-        "Programming language that enables interactive web pages and is an essential part of web applications.",
-    },
-    {
-      title: "TypeScript",
-      icon: tsIcon,
-      detail:
-        "TypeScript, a superset of JavaScript that adds static typing abilities to the language.",
-    },
-    {
-      title: "React",
-      icon: reactIcon,
-      detail:
-        "JavaScript library for building user interfaces, particularly single-page applications.",
-    },
-    {
-      title: "Redux",
-      icon: reduxIcon,
-      detail:
-        "Predictable state container for JavaScript apps, often used with React.",
-    },
-    {
-      title: "Firebase",
-      icon: firebaseIcon,
-      detail:
-        "Platform developed by Google for creating mobile and web applications.",
-    },
-    {
-      title: "Sass",
-      icon: sassIcon,
-      detail:
-        "Preprocessor scripting language that is interpreted or compiled into Cascading Style Sheets (CSS)",
-    },
-  ];
+  {
+    title: "HTML 5",
+    icon: htmlIcon,
+    detail:
+      "Hypertext Markup Language, used for structuring and presenting content on the World Wide Web.",
+  },
+  {
+    title: "CSS 3",
+    icon: cssIcon,
+    detail:
+      "Cascading Style Sheets, used for describing the look and formatting of a document written in HTML.",
+  },
+  {
+    title: "JavaScript",
+    icon: jsIcon,
+    detail:
+      "Programming language that enables interactive web pages and is an essential part of web applications.",
+  },
+  {
+    title: "TypeScript",
+    icon: tsIcon,
+    detail:
+      "TypeScript, a superset of JavaScript that adds static typing abilities to the language.",
+  },
+  {
+    title: "React",
+    icon: reactIcon,
+    detail:
+      "JavaScript library for building user interfaces, particularly single-page applications.",
+  },
+  {
+    title: "Redux",
+    icon: reduxIcon,
+    detail:
+      "Predictable state container for JavaScript apps, often used with React.",
+  },
+  {
+    title: "Firebase",
+    icon: firebaseIcon,
+    detail:
+      "Platform developed by Google for creating mobile and web applications.",
+  },
+  {
+    title: "Next",
+    icon: nextIcon,
+    detail:
+      "Next is React framework that provides server-side rendering and static website generation.",
+  },
+];
 
 export default techs;
